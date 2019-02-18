d91424eaaa0ff3a08102f9e1a4dbb7ae437f7814
lex.xu <shuangrain07@gmail.com>
2019-02-18 23:24:07 +0800

    change

---
+++ 637. Average of Levels in Binary Tree/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _637._Average_of_Levels_in_Binary_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(2147483647)
+            {
+                left = new TreeNode(2147483647),
+                right = new TreeNode(2147483647)
+            };
+
+            Solution solution = new Solution();
+            var ans = solution.AverageOfLevels(root);
+            Console.WriteLine($"ans:{string.Join(',', ans)}");
+            Console.ReadKey();
+        }
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int x) { val = x; }
+     * }
+     */
+    public class Solution
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            IList<KeyValuePair<int, long>> nodeValues = new List<KeyValuePair<int, long>>();
+            calcNodeValue(0, root, nodeValues);
+
+            IList<double> results = new List<double>();
+
+            int maxLevel = nodeValues.Max(x => x.Key);
+            for (int i = 0; i <= maxLevel; i++)
+            {
+                var targetLevelItems = nodeValues.Where(x => x.Key == i);
+                int count = targetLevelItems.Count();
+                long sum = targetLevelItems.Sum(x => x.Value);
+
+                results.Add(sum / (double)count);
+            }
+
+            return results;
+        }
+
+        private void calcNodeValue(int level, TreeNode node, IList<KeyValuePair<int, long>> nodeValues)
+        {
+            nodeValues.Add(new KeyValuePair<int, long>(level, node.val));
+
+            if (node.left != null)
+            {
+                calcNodeValue(level + 1, node.left, nodeValues);
+            }
+
+            if (node.right != null)
+            {
+                calcNodeValue(level + 1, node.right, nodeValues);
+            }
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
---
+++ 949. Largest Time for Given Digits/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _949._Largest_Time_for_Given_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var ans = solution.LargestTimeFromDigits(new int[] { 1, 2, 3, 4 });
+            Console.WriteLine($"ans:{ans}");
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public string LargestTimeFromDigits(int[] A)
+        {
+            Array.Sort(A);
+            for (int i = 23; i >= 0; i--)
+            {
+                for (int j = 59; j >= 0; j--)
+                {
+                    string hour = i.ToString("00");
+                    string min = j.ToString("00");
+                    int[] tmp = new int[]
+                    {
+                        int.Parse(hour[0].ToString()),
+                        int.Parse(hour[1].ToString()),
+                        int.Parse(min[0].ToString()),
+                        int.Parse(min[1].ToString())
+                    };
+
+                    Array.Sort(tmp);
+                    if (A[0] == tmp[0] &&
+                        A[1] == tmp[1] &&
+                        A[2] == tmp[2] &&
+                        A[3] == tmp[3])
+                    {
+                        return hour + ":" + min;
+                    }
+                }
+            }
+
+            return "";
+        }
+    }
+}
